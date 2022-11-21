59cc14fd0ad0b3d7769816c5be3dcf3a84c01045
daseul <daseul727@gmail.com>
2022-11-21 22:51:44 +0900

    [hello spring] 의존관계 생성

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) { //서비스부를때마다 new 하지않고 외부에서 넣어주는것을 di 라고함
         this.memberRepository = memberRepository;
     }
